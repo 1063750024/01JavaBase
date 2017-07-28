0511d34a7c0b660ba62cc419238f3184486ccf57
xiaodoufu <501804292@qq.com>
2017-07-28 16:15:29 +0800

    选择结构

---
+++ 03IfJava/src/cn/bdqn/test/Test.java
@@ -0,0 +1,50 @@
+package cn.bdqn.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/**
+		int num1 = 5;
+		int num2 = 6;
+		if (num1++ == num2 && ++num1 == num2) {
+			System.out.println("ÄãÓ®ÁË");
+		}
+		System.out.println(num1);
+		System.out.println(++num2);
+		
+		
+		int num1 = 5;
+		int num2 = 6;
+		if (num1++ == num2 || ++num1 == num2) {
+			System.out.println("ÄãÓ®ÁË");
+		}
+		System.out.println(num1);
+		System.out.println(++num2);
+		
+		
+		
+		int num1 = 5;
+		int num2 = 6;
+		if ( ++num1 == num2 | num1++ == num2 ) {
+			System.out.println("ÄãÓ®ÁË");
+		}
+		System.out.println(num1);
+		System.out.println(++num2);
+		
+		int num1 = 5;
+		int num2 = 6;
+		if ( ++num1 == num2 & num1++ == num2 ) {
+			System.out.println("ÄãÓ®ÁË");
+		}
+		System.out.println(++num2);
+		
+		 if(num1=num2){  //±àÒë±¨´í
+		    System.out.println(num1);
+		 }else{
+		   System.out.println(num2);
+		 }
+		*/
+
+	}
+
+}
